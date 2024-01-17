be808704fdb8303e264862db1394b9f03c5897d4
Rayan <90398515+RayanBGD@users.noreply.github.com>
2024-01-17 15:02:38 +0100

    ADD name of flowers

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ class IrisInput(BaseModel):
     petal_length: float
     petal_width: float
 
+class_names = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+
 @app.post("/predict")
 def pred_iris(iris_input: IrisInput):
     features = [
         [iris_input.sepal_length, iris_input.sepal_width, iris_input.petal_length, iris_input.petal_width]
     ]
-    pred = model.predict(features)
-    return {"prediction": int(pred[0])}
+    #pred = model.predict(features)
+    pred_class = int(model.predict(features)[0])
+
+    pred_name = class_names.get(pred_class, 'Classe inconnue')
+
+    #return {"prediction": int(pred[0])}
+    return {"prediction": pred_name}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
