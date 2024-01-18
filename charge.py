dc1779cc78cd65e007b2de4867fdecb75acac0ac
mimi <michel.hu@hotmail.fr>
2024-01-18 11:33:42 +0100

    test charge

--- charge.py
+++ charge.py
@@ -1,12 +1,13 @@
 import requests
+import random
 from concurrent.futures import ThreadPoolExecutor
 
 def send_request(index):
     data = {
-        "sepal_length": 5.1,
-        "sepal_width": 3.5,
-        "petal_length": 1.4,
-        "petal_width": 0.2
+        "sepal_length": round(random.uniform(4.3, 7.9), 1),
+        "sepal_width": round(random.uniform(2.0, 4.4), 1),
+        "petal_length": round(random.uniform(1.0, 6.9), 1),
+        "petal_width": round(random.uniform(0.1, 2.5), 1)
     }
     url = 'https://grp6-container-app.salmonisland-ae09a70e.francecentral.azurecontainerapps.io/predict'
     
