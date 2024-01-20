b7fdd9a2c504ec0478644d2817d70bec473d5300
lizemmagg <115583430+lizemmagg@users.noreply.github.com>
2024-01-20 17:09:05 +0100

    Update app.py
    counters added

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import joblib
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 import uvicorn
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
+from fastapi.responses import StreamingResponse
+
+
 
 app = FastAPI()
 model = joblib.load("knn-model.joblib")
@@ -14,6 +19,10 @@ class IrisInput(BaseModel):
 
 class_names = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
 
+setosa_counter = Counter("model_prediction_setosa_total", "Number of predictions for setosa")
+versicolor_counter = Counter("model_prediction_versicolor_total", "Number of predictions for versicolor")
+virginica_counter = Counter("model_prediction_virginica_total", "Number of predictions for virginica")
+
 @app.post("/predict")
 def pred_iris(iris_input: IrisInput):
     features = [
@@ -24,8 +33,37 @@ def pred_iris(iris_input: IrisInput):
 
     pred_name = class_names.get(pred_class, 'Classe inconnue')
 
+    
+    if pred_name == 'setosa':
+        setosa_counter.inc()
+    elif pred_name == 'versicolor':
+        versicolor_counter.inc()
+    elif pred_name == 'virginica':
+        virginica_counter.inc()
+
     #return {"prediction": int(pred[0])}
     return {"prediction": pred_name}
+
+@app.get("/metrics")
+async def get_metrics():
+    async def generate():
+        metrics_data = generate_latest()
+        yield metrics_data
+
+    return StreamingResponse(content=generate(), media_type=CONTENT_TYPE_LATEST)
+
+
+
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app)
+
+
+instrumentator.expose(app, include_in_schema=False, should_gzip=True)
+
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
